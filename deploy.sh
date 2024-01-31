d89b0dc9b7a41fdf7b4153d1fa2786862862c838
David Geo Holmes <geometryzen@gmail.com>
2024-01-31 11:50:59 -0500

    Evergreen

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#
+# Usage:
+#
+# bash ./deploy.sh [MESSAGE] [VERSION]
+#
+
+if [[ -z $1 ]]; then
+    echo "Please enter a commit message"
+    read MESSAGE
+else
+    MESSAGE=$1
+fi
+
+if [[ -z $2 ]]; then
+    echo "Please enter semantic version"
+    read VERSION
+else
+    VERSION=$2
+fi
+
+npm pkg set version="$VERSION"
+
+npm install
+npm update
+npm run build
+git status
+git add --all
+git commit -m "'$MESSAGE'"
+git push origin main
